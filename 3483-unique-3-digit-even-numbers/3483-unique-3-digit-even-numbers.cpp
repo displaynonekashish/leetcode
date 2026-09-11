fc7c4cc84f544d0bc849d94d1c35b8bd535b35eb
kashish <163969715+displaynonekashish@users.noreply.github.com>
2026-09-11 11:47:34 +0530

    Time: 4 ms (79.08%), Space: 28.7 MB (93.75%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -3,41 +3,44 @@ public:
     int totalNumbers(vector<int>& digits) {
         vector<int> freq(10, 0);
 
-        // Count how many times each digit is available
         for (int d : digits) {
             freq[d]++;
         }
 
         int ans = 0;
 
-        for (int num = 100; num <= 998; num += 2) {
-            int x = num;
+        for (int num = 100; num <= 999; num++) {
+            if (num % 2 != 0)
+                continue;
 
-            int ones = x % 10;
-            x /= 10;
+            int a = num / 100;
+            int b = (num / 10) % 10;
+            int c = num % 10;
 
-            int tens = x % 10;
-            x /= 10;
+            if (freq[a] == 0)
+                continue;
 
-            int hundreds = x % 10;
+            if (freq[b] == 0)
+                continue;
 
-            vector<int> need(10, 0);
-            need[ones]++;
-            need[tens]++;
-            need[hundreds]++;
+            if (freq[c] == 0)
+                continue;
 
-            bool possible = true;
-
-            for (int d = 0; d <= 9; d++) {
-                if (need[d] > freq[d]) {
-                    possible = false;
-                    break;
-                }
+           
+            if (a == b && b == c) {
+                if (freq[a] < 3) continue;
+            }
+            else if (a == b) {
+                if (freq[a] < 2) continue;
+            }
+            else if (a == c) {
+                if (freq[a] < 2) continue;
+            }
+            else if (b == c) {
+                if (freq[b] < 2) continue;
             }
 
-            if (possible) {
-                ans++;
-            }
+            ans++;
         }
 
         return ans;
